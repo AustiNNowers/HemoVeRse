575a9f0f5b9b0bc55150d7e7eadb8626702ef049
AustiNNowers <luishenriquesilvabranco@gmail.com>
2025-08-30 16:14:46 -0300

    Mexendo com o raycast do VR

--- CardBoardVR-main/Assets/Scripts/InteragirComRaycast.cs
+++ CardBoardVR-main/Assets/Scripts/InteragirComRaycast.cs
@@ -6,7 +6,7 @@ public class InteragirComRaycast : MonoBehaviour
 
     void Update()
     {
-        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        /*Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit))
@@ -23,6 +23,28 @@ public class InteragirComRaycast : MonoBehaviour
         else
         {
             objetoRenderer.material.color = Color.white;
-        }
+        }*/
+    }
+
+    public void OnPointerEnter()
+    {
+        objetoRenderer.material.color = Color.red;
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it stops gazing at this GameObject.
+    /// </summary>
+    public void OnPointerExit()
+    {
+        objetoRenderer.material.color = Color.green;
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
+    /// is touched.
+    /// </summary>
+    public void OnPointerClick()
+    {
+        objetoRenderer.material.color = Color.blue;
     }
 }
